09232bc54a216ad9981e62e391c35f4c748d176a
Bohdan <yulia.pakhachuk@gmail.com>
2022-12-11 16:55:56 +0100

    fixed container

--- src/components/App/StyledApp.jsx
+++ src/components/App/StyledApp.jsx
@@ -4,6 +4,7 @@ export const StyledApp = styled.div`
   max-width: 60%;
     font-size: 16px;
     font-weight: 600;
+    height: 100vh;
     font-style: italic;
     color: #000000;
     margin: 10px auto;
