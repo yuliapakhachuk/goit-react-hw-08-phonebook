7fe747866665bef6370efb5e7aaa1ce6820d96e4
Bohdan <yulia.pakhachuk@gmail.com>
2022-12-11 14:46:16 +0100

    fixed router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,59 @@
-import { StyledApp } from './StyledApp';
-import { ContactForm } from '../Form/Form';
-import { Filter } from '../Filter/Filter';
-import { ContactList } from '../ContactList/ContactList';
+import { Route, Routes } from 'react-router-dom';
+import { refreshUser } from 'redux/auth/auth-operations';
+import { useDispatch } from 'react-redux';
+import { useEffect, lazy } from 'react';
+import { useAuth } from 'redux/auth/auth-selectors';
+import PublicRoute from 'components/PublicRoute';
+import PrivateRoute from 'components/PrivateRoute';
+
+import AppBar from 'components/AppBar/AppBar';
+const ContactsView = lazy(() => import('views/ContactsView/ContactsView'));
+const RegisterView = lazy(() => import('views/RegisterView'));
+const LoginView = lazy(() => import('views/LoginView'));
 
 export const App = () => {
-  return (
-    <StyledApp>
-      <h1>Phone book</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
-    </StyledApp>
+  const dispatch = useDispatch();
+  const { isRefreshingUser } = useAuth();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshingUser ? (
+    <h1>Refreshing user...</h1>
+  ) : (
+    <Routes>
+      <Route path="/" element={<AppBar />}>
+        <Route
+          index
+          element={<PublicRoute component={<h1>Welcome to contact book</h1>} />}
+        />
+        <Route
+          path="contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsView />} />
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <PublicRoute
+              restricted
+              redirectTo="/contacts"
+              component={<RegisterView />}
+            />
+          }
+        />
+        <Route
+          path="login"
+          element={
+            <PublicRoute
+              restricted
+              redirectTo="/contacts"
+              component={<LoginView />}
+            />
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
